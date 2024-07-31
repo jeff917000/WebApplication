d313e6f1077d38b5e748fcceaeda2c0aedf29229
Cyric <cyric@localhost.com>
2024-08-01 01:52:15 +0800

    密碼加密方法修改

--- LoginDemo/Utilities/Encryption_Library.cs
+++ LoginDemo/Utilities/Encryption_Library.cs
@@ -13,23 +13,24 @@ namespace LoginDemo.Utilities
 
             //雖然不需要，但還是加上判斷了。
             if (string.IsNullOrEmpty(str)) return null;
-
+            byte[] inBlock = Encoding.UTF8.GetBytes(str);
             using (Aes strAes = Aes.Create())
             {
                 strAes.Key = key;
                 strAes.IV = iv;
                 ICryptoTransform encryptor = strAes.CreateEncryptor(strAes.Key, strAes.IV);
-                using (MemoryStream msEncrypt = new MemoryStream())
-                {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            swEncrypt.Write(str);
-                        }
-                        encrypted = msEncrypt.ToArray();
-                    }
-                }
+                encrypted = encryptor.TransformFinalBlock(inBlock, 0, inBlock.Length);
+                //using (MemoryStream msEncrypt = new MemoryStream())
+                //{
+                //    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                //    {
+                //        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                //        {
+                //            swEncrypt.Write(str);
+                //        }
+                //        encrypted = msEncrypt.ToArray();
+                //    }
+                //}
             };
             return Convert.ToBase64String(encrypted);
         }
