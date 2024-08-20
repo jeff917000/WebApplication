fe13c7a8b7767bba0de77e7e0dc983dcf2a4037e
CYRIC <jeff917000@hotmail.com>
2024-08-20 11:26:17 +0800

    chage UserTableItemAttributes.cs

--- LoginDemo/Models/UserTableItemAttributes.cs
+++ LoginDemo/Models/UserTableItemAttributes.cs
@@ -8,11 +8,11 @@ namespace LoginDemo.Models
         public int UserId { get; set; }
         [Required]
         [Display(Name = "帳號")]
-        public string UserName { get; set; }
+        public required string UserName { get; set; }
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "密碼")]
-        public string UserPassword { get; set; }
+        public required string UserPassword { get; set; }
         [Required]
         [Display(Name = "權限")]
         public int UserRole { get; set; }
